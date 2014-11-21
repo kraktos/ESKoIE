5999a8975b7fc97f46bfc1d847594d2e8a5cad1f
kraktos <arnab.dutta@gmail.com>
2014-11-21 10:29:20 +0100

    adding gs creation

---
+++ src/main/java/code/dws/experiment/goldstandard/RelationCreation.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package code.dws.experiment.goldstandard;
+
+/**
+ * Distant supervision based 
+ * @author adutta
+ *
+ */
+public class RelationCreation {
+
+	/**
+	 * 
+	 */
+	public RelationCreation() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
